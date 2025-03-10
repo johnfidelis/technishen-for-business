730a2f4664a7f338672973130e1f577188205a22
johnfidelis <fidelisjohn836@gmail.com>
2025-03-10 16:49:02 +0100

    fix ui

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,16 +1,18 @@
 import { Cookies } from 'react-cookie'
 const cookies = new Cookies()
-const businessId = cookies.get('selectedBusinessId')
+const getBusinessId = () => cookies.get('selectedBusinessId')
+
+// const business() = cookies.get('selectedgetBusinessId()')
 
 export const POST_ENDPOINTS = {
   BUSINESS_PROFILE: '/business-profile/',
   CREATE_INTERNAL_TICKET: '/create-web-ticket/',
-  CREATE_EXTERNAL_TICKET: `/b2c/business/${businessId}/create-ticket/`,
-  CREATE_EMPLOYEE: `/create-employee/${businessId}/`,
-  CREATE_CUSTOMER: `/create-customer/${businessId}/`,
+  CREATE_EXTERNAL_TICKET: `/b2c/business/${getBusinessId()}/create-ticket/`,
+  CREATE_EMPLOYEE: `/create-employee/${getBusinessId()}/`,
+  CREATE_CUSTOMER: `/create-customer/${getBusinessId()}/`,
 
   RESEND_ACCESS_CODE: '/resend-access-code/',
-  CREATE_FULFILLER_GROUP: `/create-fulfiller-group/${businessId}/`,
+  CREATE_FULFILLER_GROUP: `/create-fulfiller-group/${getBusinessId()}/`,
   CREATE_CATEGORY_AND_SUB_CATEGORY: `/create-category-and-sub-category/`,
   ADD_EMPLOYEE_TO_FULFILER_GROUP: (groupId) =>
     `/add-employees-to-fulfiller-group/${groupId}/`,
@@ -19,10 +21,10 @@ export const POST_ENDPOINTS = {
 export const PATCH_ENDPOINTS = {
   BUSINESS_OWNER_PROFILE: '/business-owner-profile/',
   UPDATE_EMPLOYEE_ROLE: (employeeId) =>
-    `/update-employee-role/${businessId}/${employeeId}/`,
+    `/update-employee-role/${getBusinessId()}/${employeeId}/`,
   UPDATE_CATALOG: (serviceId) => `/update-category/${serviceId}/`,
   UPDATE_SUB_CATALOG: (serviceId) => `/update-sub-category/${serviceId}/`,
-  CUSTOMIZE_UI: `/customize-UI/${businessId}/`,
+  CUSTOMIZE_UI: `/customize-UI/${getBusinessId()}/`,
 }
 
 export const AUTH_ENDPOINTS = {
@@ -39,30 +41,31 @@ export const GET_ENDPOINTS = {
   CALLER: '/search-caller/',
   TICKET_CATEGORY_TO_BOOK: '/ticket-category-to-book/',
   FULFILLER_GROUP_TO_BOOK: '/fulfiller-group-to-book/',
-  ALL_TICKETS: `/all-tickets/${businessId}/`,
-  COLORS: `/get-colors/${businessId}/`,
-  ALL_CATEGORIES: `/all-categories/${businessId}/`,
-  ALL_OUTSOURCED_TICKETS: `/b2c/get-outsourced-tickets/${businessId}`,
+  ALL_TICKETS: `/all-tickets/${getBusinessId()}/`,
+  COLORS: `/get-colors/${getBusinessId()}/`,
+  ALL_CATEGORIES: `/all-categories/${getBusinessId()}/`,
+  ALL_OUTSOURCED_TICKETS: `/b2c/get-outsourced-tickets/${getBusinessId()}`,
   TECHNISHEN_BOOKABLE_SERVICES: '/b2c/bookable-services/',
 
-  VIEW_TICKETS: (ticketId) => `/the-ticket/${businessId}/${ticketId}/`,
+  VIEW_TICKETS: (ticketId) => `/the-ticket/${getBusinessId()}/${ticketId}/`,
   VIEW_OUTSOURCED_TICKETS: (ticketId) =>
     `/b2c/customer-or-outsourced-ticket-details/${ticketId}/`,
   TICKET_NOTES: (ticketId) => `/ticket/${ticketId}/notes/`,
   SUB_CATEGORY: (categoryId) => `/all-sub-categories/${categoryId}/`,
   // CUSTOMER_TICKET_HISTORY: (customerId) => {
-  //   const businessId = cookies.get('selectedBusinessId') // Fetch latest businessId
-  //   return `/customer-ticket-history/${customerId}/${businessId}`
+  //   const getBusinessId() = cookies.get('selectedgetBusinessId()') // Fetch latest getBusinessId()
+  //   return `/customer-ticket-history/${customerId}/${getBusinessId()}`
   // },
   CUSTOMER_TICKET_HISTORY: (customerId) =>
-    `/customer-ticket-history/${customerId}/${businessId}`,
+    `/customer-ticket-history/${customerId}/${getBusinessId()}`,
   EMPLOYEE_TICKET_HISTORY: (customerId) =>
-    `/all-tickets-assigned-to-employee/${customerId}/${businessId}`,
+    `/all-tickets-assigned-to-employee/${customerId}/${getBusinessId()}`,
 
-  ALL_EMPLOYEE: `/all-employees/${businessId}`,
-  ALL_CUSTOMER: `/all-employees/${businessId}`,
-  ALL_FULFILLER_GROUPS: `/all-fulfiller-groups/${businessId}`,
-  GET_EMPLOYEE: (employeeId) => `/get-employee/${businessId}/${employeeId}/`,
+  ALL_EMPLOYEE: `/all-employees/${getBusinessId()}`,
+  ALL_CUSTOMER: `/all-employees/${getBusinessId()}`,
+  ALL_FULFILLER_GROUPS: `/all-fulfiller-groups/${getBusinessId()}`,
+  GET_EMPLOYEE: (employeeId) =>
+    `/get-employee/${getBusinessId()}/${employeeId}/`,
   GET_FULFILLER_GROUP: (groupId) =>
-    `get-fulfiller-group/${businessId}/${groupId}/`,
+    `get-fulfiller-group/${getBusinessId()}/${groupId}/`,
 }
