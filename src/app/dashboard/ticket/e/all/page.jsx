8de3e8e89aa051f620dd0ab2db7cb7180422d238
johnfidelis <fidelisjohn836@gmail.com>
2025-03-14 18:56:39 +0100

    add

--- src/app/dashboard/ticket/e/all/page.jsx
+++ src/app/dashboard/ticket/e/all/page.jsx
@@ -79,7 +79,7 @@ export default function Page() {
             fontFamily: 'Inter, sans-serif',
           }}
         >
-          All Tickets{`${number}`}
+          All Tickets {`${number}`}
           <Tooltip
             title="This page displays all open tickets that are currently unassigned. You can take action to assign or manage these tickets."
             arrow
--- src/component/TicketTable.jsx
+++ src/component/TicketTable.jsx
@@ -270,8 +270,8 @@ const TicketTable = ({ filterType, setNumber }) => {
           >
             Show Outsourced Tickets
             {showOutsourced
-              ? `(${outsourcedTicketsData?.length})`
-              : `(${ticketCounts?.[filterType]})`}
+              ? `(${outsourcedTicketsData?.length || 0})`
+              : `(${ticketCounts?.[filterType] || 0})`}
           </Typography>
         }
       />
